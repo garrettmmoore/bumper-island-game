3f81450769715e11de74f53d6b8a3c61d6b2c9a8
Garrett Moore <28462557+garrettmmoore@users.noreply.github.com>
2022-05-30 00:58:00 -0700

    Prevent camera from rotating on Game Over screen

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
     [NonSerialized] private const float RotationSpeed = 70;
+    public GameManager gameManager;
 
     // Update is called once per frame
     private void Update()
     {
-        var horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * RotationSpeed * Time.deltaTime);
+        if (gameManager.isGameActive)
+        {
+            var horizontalInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up, horizontalInput * RotationSpeed * Time.deltaTime);
+        }
     }
 }
